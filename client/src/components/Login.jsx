ca24475a5593b4832bd4e13bc2e8655e1ded6e70
Amaresh Maharana <amareshcoder@gmail.com>
2025-03-25 19:18:36 +0530

    Enhance login and navbar components with user-specific data and logout functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -17,14 +17,14 @@ const Login = () => {
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-  
+
     try {
       if (state === "Login") {
         const { data } = await axios.post(`${backendUrl}/user/login`, {
           email,
           password,
         });
-  
+
         if (data.success) {
           setToken(data.token);
           setUser(data.user);
@@ -39,7 +39,7 @@ const Login = () => {
           email,
           password,
         });
-  
+
         if (data.success) {
           setToken(data.token);
           setUser(data.user);
@@ -65,7 +65,8 @@ const Login = () => {
   return (
     <>
       <div className="fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
-        <motion.form onSubmit={onSubmitHandler}
+        <motion.form
+          onSubmit={onSubmitHandler}
           initial={{ opacity: 0.2, y: 50 }}
           transition={{ duration: 0.3 }}
           whileInView={{ opacity: 1, y: 0 }}
